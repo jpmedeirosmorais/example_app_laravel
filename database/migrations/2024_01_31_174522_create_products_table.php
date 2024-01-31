fb1104dee8f966c136beca93622fcc10a3bc8345
João P M Morais <paulomedeiros5000@gmail.com>
2024-01-31 15:39:21 -0300

    Fixed category to singular and use 'foreingId'

--- database/migrations/2024_01_31_174522_create_products_table.php
+++ database/migrations/2024_01_31_174522_create_products_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->uuid('id');
             $table->char('name', 255);
-            $table->uuid('categorie_id');
-            $table->foreign('categorie_id')->references('id')->on('categories');
+            $table->uuid('category_id');
+            $table->foreignId('category_id')->constrained();
             $table->decimal('price', 8, 2);
             $table->longText('description');
             $table->softDeletes();
             $table->timestamps();
             $table->enum('status', ['ativo', 'inativo']);
+
         });
     }
 
