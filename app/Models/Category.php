cd1d6d37b2f203013f842168770c856b23d622be
João P M Morais <paulomedeiros5000@gmail.com>
2024-02-01 16:46:22 -0300

    To create CategoryFactory

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,39 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
 
 class Category extends Model
 {
+
     use HasFactory, HasUuids;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'slug'];
 
-    public function products(): \Illuminate\Database\Eloquent\Relations\HasMany
+    public function products(): HasMany
     {
         return $this->hasMany(Product::class);
     }
 
-    protected function name(): Attribute
+    public function slug(): string
     {
-
-        return Attribute::make(
-            get: fn ($value) => Str::title($value),
-            set: fn ($value) => Str::title($value)
-        );
-    }
-
-    protected function slug($name): Attribute
-    {
-        return Attribute::make(
-            get: fn ($value) => Str::slug($this->Attributes->get($name)),
-            set: fn ($value) => Str::slug($this->Attributes->get($name))
-        );
-
+         return $this->slug = Str::slug($this->name, '-');
     }
 
 }
